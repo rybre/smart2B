92756077f1748425077faec2b51ab01a56f28dfc
rybre <qjaqns1143@o365.jeiu.ac.kr>
2019-04-18 16:13:30 +0900

    메이븐 생성

---
+++ MyBootApp/src/main/java/2018141040/App.java
@@ -0,0 +1,13 @@
+package 2018141040;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
